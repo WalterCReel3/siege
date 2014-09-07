f800e4915bc1183d1f41f5e4fa4ba8cba4fb08d3
Walter Reel <WalterCReel3rd@gmail.com>
2014-09-07 09:36:55 -0400

    handle invalid cookies

--- server/siege/decorators.py
+++ server/siege/decorators.py
@@ -2,11 +2,14 @@ from flask import request
 from flask import redirect
 from functools import wraps
 
+from siege.models import Device
 
 def ensure_device(f):
     @wraps(f)
     def decorated():
-        if not request.cookies.get('device_id'):
+        device_id = request.cookies.get('device_id')
+        if device_id and Device.query.get(device_id):
+            return f()
+        else:
             return redirect('/', code=302)
-        return f()
     return decorated
--- server/siege/views/index.py
+++ server/siege/views/index.py
@@ -8,8 +8,9 @@ from siege.models import Device
 @app.route('/')
 def index():
     response = make_response(render_template('index.html'))
-    if not request.cookies.get('device_id'):
-            new_device = Device.create(request.remote_addr,
-                                       request.user_agent)
-            response.set_cookie('device_id', new_device.id)
+    device_id = request.cookies.get('device_Id')
+    if (not device_id) or (not Device.query.get(device_id)):
+        new_device = Device.create(request.remote_addr,
+                                   request.user_agent)
+        response.set_cookie('device_id', new_device.id)
     return response
